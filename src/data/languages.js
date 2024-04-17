364c502549501c403b403671f5a0fbf40f2fc6ea
fatihozcandev <fatih-ozcan@windowslive.com>
2024-04-17 21:56:03 +0200

    initial

---
+++ src/data/languages.js
@@ -0,0 +1,174 @@
+import filmProject from "../assests/filmProject.png";
+
+export const languages = {
+  en: {
+    title: "Frontend Developer",
+    description:
+      " With over two years of dedicated experience in executive management within the export and import domainand many years in hospitality, I bring a wealth of expertise in overseeing international trade operations. Havingrecently relocated to Germany, I am pivoting towards a new career trajectory in software programming. Currentlyengaging programming tutors to enhance my skills, I am poised to leverage my managerial acumen andadaptability to thrive in this dynamic and innovative field.",
+    profile: "Profile",
+    skill: "Skills",
+    projects: "Projects",
+    viewSite: "View Site",
+    aboutSection: {
+      section: "About Me",
+      name: "Fatih Özcan",
+      bdaytitle: "Birthday",
+      bday: "23.02.1993",
+      cityTitle: "City",
+      city: "Dessau-Roßlau",
+      educationTitle: "Education",
+      education: {
+        uni: "Anadolu University - English Language Teaching",
+        type: "Bachelor's degree",
+        year: "2016",
+      },
+      role: "Front End Developer",
+      roleTitle: "Preferred Role",
+      aboutMe:
+        "With over two years of dedicated experience in executive management within the export and import domainand many years in hospitality, I bring a wealth of expertise in overseeing international trade operations. Havingrecently relocated to Germany, I am pivoting towards a new career trajectory in software programming. Currentlyengaging programming tutors to enhance my skills, I am poised to leverage my managerial acumen andadaptability to thrive in this dynamic and innovative field.",
+    },
+    footerSection: {
+      main: "Let’s work together on your next product.",
+    },
+    headerData: {
+      logoText: "F.Ö.",
+      navigationLinks: ["Skills", "Projects", "Contact Me"],
+    },
+    projectsData: [
+      {
+        id: 1,
+        title: "Pizza",
+        imageUrl:
+          "https://www.santosgrills.de/grill-magazin/wp-content/uploads/2023/07/SANTOS-Grill-Magazin-Peperoni-Pizza-Rezept.jpg",
+        description:
+          "It is a Single Page Application designed to bring food to developers.",
+        technologies: ["React", "Redux", "Axios"],
+        githubLink: "https://github.com/fatihozcandev/WIT-s7-SPA-",
+        viewSiteLink: "https://wit-s7-spa.vercel.app/siparis",
+      },
+      {
+        id: 2,
+        title: "Bookstore",
+        imageUrl:
+          "https://media.istockphoto.com/id/1218656325/photo/laptop-with-online-library-realistic-3d-rendering.jpg?s=612x612&w=0&k=20&c=R9yd2DvnzDfmJvPEbpTznc89XwX7SD3k8kAeUU3UnMU=",
+        description: "It is a project designed to order books.",
+        technologies: ["React", "Redux", "Axios"],
+        githubLink: "#",
+        viewSiteLink: "https://bookstore-s11g1.onrender.com/",
+      },
+      {
+        id: 3,
+        title: "Grid Widget",
+        imageUrl: "https://i.stack.imgur.com/OteU7.png",
+        description:
+          "It is an application that allows working on grids and can dynamically navigate within the grid.",
+        technologies: ["React", "Axios"],
+        githubLink:
+          "https://github.com/fatihozcandev/WITs9sprintProject-gridWidget-reactApiPostman",
+        viewSiteLink: "#",
+      },
+    ],
+    skillsData: [
+      {
+        title: "JavaScript",
+        description:
+          "JavaScript (JS) is a high-level, interpreted programming language primarily used for adding interactivity and dynamic content to web pages.",
+      },
+      {
+        title: "React.Js",
+        description:
+          "Node.js is an open-source, cross-platform JavaScript runtime environment that allows developers to execute JavaScript code outside of a web browser, typically on the server side, enabling scalable and efficient server-side programming.",
+      },
+      {
+        title: "Node.Js",
+        description:
+          "React.js is an open-source JavaScript library for building user interfaces in web applications, known for its component-based architecture and efficient rendering.",
+      },
+    ],
+  },
+  tr: {
+    title: "Frontend Developer",
+    description:
+      "İki yıldan fazla süredir dış ticaret alanında yöneticilik deneyimim ve çok sayıda yıl süren konaklama sektöründeki çalışmalarım sayesinde uluslararası ticaret operasyonlarını denetleme konusunda geniş bir uzmanlık birikimine sahibim. Yakın zamanda Almanya'ya taşındım ve şimdi yazılım programlamaya doğru yeni bir kariyer rotasına yöneliyorum. Şu anda programlama öğretmenleriyle çalışarak becerilerimi geliştiriyorum ve yönetim yeteneklerimle ve uyum kabiliyetimle bu dinamik ve yenilikçi alanın içinde başarılı olmayı hedefliyorum.",
+    profile: "Profil",
+    skill: "Yetenekler",
+    projects: "Projeler",
+    viewSite: "Projeyi görüntüle",
+    aboutSection: {
+      section: "Hakkımda",
+      name: "Fatih Özcan",
+      bdaytitle: "Birthday",
+      bday: "23.02.1993",
+      cityTitle: "İkamet Şehri",
+      city: "Dessau-Roßlau",
+      educationTitle: "Eğitim Durumu",
+      education: {
+        uni: "Anadolu Üniversitesi - İngilizce Öğretmenliği",
+        type: "Lisans",
+        year: "2016",
+      },
+      role: "Front-End Geliştirici",
+      roleTitle: "Tercih Ettiği Rol",
+      aboutMe:
+        "İki yıldan fazla süredir dış ticaret alanında yöneticilik deneyimim ve çok sayıda yıl süren konaklama sektöründeki çalışmalarım sayesinde uluslararası ticaret operasyonlarını denetleme konusunda geniş bir uzmanlık birikimine sahibim. Yakın zamanda Almanya'ya taşındım ve şimdi yazılım programlamaya doğru yeni bir kariyer rotasına yöneliyorum. Şu anda programlama öğretmenleriyle çalışarak becerilerimi geliştiriyorum ve yönetim yeteneklerimle ve uyum kabiliyetimle bu dinamik ve yenilikçi alanın içinde başarılı olmayı hedefliyorum.",
+    },
+    footerSection: {
+      main: "Bir sonraki ürününüz üzerinde birlikte çalışalım.",
+    },
+    headerData: {
+      logoText: "F.Ö.",
+      navigationLinks: ["Yetenekler", "Projeler", "Bana ulaş"],
+    },
+    projectsData: [
+      {
+        id: 1,
+        title: "Pizza",
+        imageUrl:
+          "https://www.santosgrills.de/grill-magazin/wp-content/uploads/2023/07/SANTOS-Grill-Magazin-Peperoni-Pizza-Rezept.jpg",
+        description:
+          "Yazılımcılara yiyecek getirmek için tasarlanmış bir Single Page Application'dır.",
+        technologies: ["React", "Redux", "Axios"],
+        githubLink: "https://github.com/fatihozcandev/WIT-s7-SPA-",
+        viewSiteLink: "https://wit-s7-spa.vercel.app/siparis",
+      },
+      {
+        id: 2,
+        title: "Bookstore",
+        imageUrl:
+          "https://media.istockphoto.com/id/1218656325/photo/laptop-with-online-library-realistic-3d-rendering.jpg?s=612x612&w=0&k=20&c=R9yd2DvnzDfmJvPEbpTznc89XwX7SD3k8kAeUU3UnMU=",
+        description: "Kitap siparişi üzerine tasarlanmış bir projedir.",
+        technologies: ["React", "Redux", "Axios"],
+        githubLink: "#",
+        viewSiteLink: "https://bookstore-s11g1.onrender.com/",
+      },
+      {
+        id: 3,
+        title: "Grid Widget Project",
+        imageUrl: "https://i.stack.imgur.com/OteU7.png",
+        description:
+          "Gridler üzerinde çalışmaya olanak sağlayan ve dinamik olarak grid içerisinde gezinebilen bir uygulamadır.",
+        technologies: ["React", "Axios"],
+        githubLink:
+          "https://github.com/fatihozcandev/WITs9sprintProject-gridWidget-reactApiPostman",
+        viewSiteLink: "#",
+      },
+    ],
+    skillsData: [
+      {
+        title: "JavaScript",
+        description:
+          "JavaScript (JS), yüksek seviyeli, yorumlanan bir programlama dilidir ve web sayfalarına etkileşim ve dinamik içerik eklemek için kullanılır.",
+      },
+      {
+        title: "React.Js",
+        description:
+          "Node.js, geliştiricilere JavaScript kodunu genellikle sunucu tarafında çalıştırmalarını sağlayan açık kaynaklı, çapraz platform JavaScript çalıştırma ortamıdır.",
+      },
+      {
+        title: "Node.Js",
+        description:
+          "React.js, web uygulamalarındaki kullanıcı arayüzlerini oluşturmak için kullanılan, bileşen tabanlı mimarisi ve verimli render etme özelliğiyle tanınan açık kaynaklı bir JavaScript kütüphanesidir.",
+      },
+    ],
+  },
+};
